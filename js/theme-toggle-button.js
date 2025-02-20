fd6c3cf2748a1b580c6f67de67cc7815708a568f
William Gagnet <wgagnet1@gmail.com>
2025-02-19 22:56:18 -0600

    completed theme toggle for index.html

--- js/theme-toggle-button.js
+++ js/theme-toggle-button.js
@@ -1,13 +1,70 @@
 const button = document.getElementById("theme-toggle-button")
+let videoElement = document.getElementById("backgroundVideo");
+let sourceElement = document.getElementById("videoSource");
 
 button.addEventListener("click", function() {
-    if (button.innerHTML.includes("Light")) {
-        button.innerHTML = '<i class="fa-solid fa-moon"></i> Dark';
-    } 
-    else if (button.innerHTML.includes("Dark")) {
-        button.innerHTML = '<i class="fa-solid fa-gears"></i> Auto';
-    }
-    else if (button.innerHTML.includes("Auto")) {
+    // dark mode -> light mode
+    if (button.innerHTML.includes("Dark")) {
+        // changes button text
         button.innerHTML = '<i class="fa-solid fa-sun"></i> Light';
+
+        // video background
+        sourceElement.src = "assets/videos/coverr-cloudy-sky-2765-1080p.mp4"
+
+        // overlay
+        document.getElementById("overlay").style.background = "rgba(0, 0, 0, 0)";
+
+        // main
+        document.getElementById("bouncer").style.color = "black";
     }
+
+    // light mode -> auto mode
+    else if (button.innerHTML.includes("Light")) {
+        // changes button text
+        button.innerHTML = '<i class="fa-solid fa-gears"></i> Auto';
+
+        // get current time
+        let currentHour = new Date().getHours(); // Get the current hour (0-23)
+
+        // Daytime (6 AM - 6 PM)
+        if (currentHour >= 6 && currentHour < 18) {
+            // video background
+            sourceElement.src = "assets/videos/coverr-cloudy-sky-2765-1080p.mp4";
+
+            // overlay
+            document.getElementById("overlay").style.background = "rgba(0, 0, 0, 0)";
+
+            // main
+            document.getElementById("bouncer").style.color = "black";
+
+        // Nighttime (6 PM - 6 AM)
+        } else {
+            // video background
+            sourceElement.src = "assets/videos/coverr-lights-swaying-in-the-evening-breeze-5160-1080p.mp4";
+
+            // overlay
+            document.getElementById("overlay").style.background = "rgba(0, 0, 0, 0.5)";
+
+            // main
+            document.getElementById("bouncer").style.color = "white";
+        }
+    }
+
+    // auto mode -> dark mode
+    else if (button.innerHTML.includes("Auto")) {
+        // changes button text
+        button.innerHTML = '<i class="fa-solid fa-moon"></i> Dark';
+
+        // video background
+        sourceElement.src = "assets/videos/coverr-lights-swaying-in-the-evening-breeze-5160-1080p.mp4"
+
+        // overlay
+        document.getElementById("overlay").style.background = "rgba(0, 0, 0, 0.5)";
+
+        // main
+        document.getElementById("bouncer").style.color = "white";
+    }
+
+    // loads new video background
+    videoElement.load();
 });
